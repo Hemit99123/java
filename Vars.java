ba1ca94f5a2d8ab09303c171ddd27c9d98be2273
Hemit Patel <hemitvpatel@gmail.com>
2025-08-16 14:22:02 -0400

    Create Vars.java

---
+++ Vars.java
@@ -0,0 +1,17 @@
+public class Vars {
+  public static void main(Strings[] args) {
+    // How to define a variable 
+    // Java variables must have types because Java is statically typed lang (you have to explicity mention data types)
+
+    /*
+    PRIMATIVE DATA TYPES:
+    These are simple data types with no methods or attributes assocaited to them, because they aren't classes.
+    */
+
+    /*
+    REFERENCE DATA TYPES:
+    These are complex data types WITH methods and attributes (sometimes), because they ARE classes which are referenced.
+    */
+
+  }
+}
