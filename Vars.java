7b80f89b5f2ba891339b6ce3d95333715a90c5ec
Hemit Patel <hemitvpatel@gmail.com>
2025-08-16 14:32:41 -0400

    added core code

--- Vars.java
+++ Vars.java
@@ -5,13 +5,23 @@ public class Vars {
 
     /*
     PRIMATIVE DATA TYPES:
-    These are simple data types with no methods or attributes assocaited to them, because they aren't classes.
+    These are simple data types with no methods or attributes associated to them, because they aren't classes.
+    Below are some examples
     */
 
+    int numOfStocks = 100; // integers 
+    char strongPwd = '1@fhfh2'; // Cannot use "" + for special characters like @#$
+    double priceInDecimal = 3.99; // big or small numbers + decimals
+    
     /*
     REFERENCE DATA TYPES:
     These are complex data types WITH methods and attributes (sometimes), because they ARE classes which are referenced.
+    Below are some examples
     */
 
+    String name = "hemit";
+
+    // For this type, we have access to different methods such as...
+    System.out.println(name.length());
   }
 }
