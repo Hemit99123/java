616cf9d973675a44ab3a8c1778db9fadc3324e86
Hemit Patel <hemitvpatel@gmail.com>
2025-08-17 17:03:51 -0400

    Update Tree.java

--- Tree.java
+++ Tree.java
@@ -15,3 +15,21 @@ public class Tree {
 
   }
 }
+
+import java.lang.Math;
+
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        for (int num: nums) {
+            int absNum = Math.abs(num);
+            
+            if (Math.abs(num)) {
+                System.out.println(num + " is double-digit");
+            }
+            
+            else if ((num >= 1 && num <= 9) || (num <= -1 && num >= -99)) {
+
+            }
+        }
+    }
+}
