da15f43ab7c8c2931f87777b7dd91630804c7021
Hemit Patel <hemitvpatel@gmail.com>
2025-08-18 19:42:18 -0400

    Update Solution.java

--- leetcode/Solution.java
+++ leetcode/Solution.java
@@ -1,23 +1,33 @@
-import java.lang.Math;
+class DigitGame {
 
-class Solution {
-
-    public int alicePts;
-    public int bobPts;
+    public int aliceSinglePts;
+    public int bobSinglePts;
+    public int bobDoublePts;
+    public int aliceDoublePts; // double digits
+    public int total;
 
     public boolean canAliceWin(int[] nums) {
-        for (int num: nums) {
-            int absNum = Math.abs(num);
-            
-            if (absNum >= 1 && absNum <= 9) {
-                alicePts += num
+        for (int num: nums) {           
+            boolean isSingle =  num >= 1 && num <= 9;
+            boolean isDouble = num >= 10 && num <= 99;
+            if (isSingle) {
+                aliceSinglePts += num;
             }
-            
-            else if (absNum >= 10 && absNum <= 99) {
-                bobPts += num;
-            } 
+
+            else if (isDouble) {
+                aliceDoublePts += num;                
+            }
+
+            total += num;
         }
 
-        System.out.println(alicePts < bobPts);
+        bobSinglePts = total - aliceSinglePts;
+        bobDoublePts = total - aliceDoublePts;
+
+        if (aliceSinglePts > bobSinglePts || aliceDoublePts > bobDoublePts) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
